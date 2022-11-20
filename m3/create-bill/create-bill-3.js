ccfad3fbee44c8e4271d0012edc5345a66b57a96
Tevis Doe <doet@mcmaster.ca>
2022-11-19 21:43:48 -0500

    remove num only input

--- m3/create-bill/create-bill-3.js
+++ m3/create-bill/create-bill-3.js
@@ -6,10 +6,6 @@ $(document).ready(function() {
         $('#image-box').attr("src", imageUrl);
     }
 
-    $(".price").keypress(function(e) {
-        if (isNaN(String.fromCharCode(e.which))) e.preventDefault();
-    });
-
     $(".continue-button").click(function() {
         if ($('.price').html()) sessionStorage.setItem('price_selected', $(".price").html());
         else sessionStorage.setItem('price_selected', '89.99')
