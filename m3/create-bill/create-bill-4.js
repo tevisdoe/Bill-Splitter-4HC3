6ba718ca5d86be3ee8b87eacad0ac7d53bcf1294
Harkeerat Kanwal <hsk532991@gmail.com>
2022-11-20 02:54:21 -0500

    Fix selected category not showing

--- m3/create-bill/create-bill-4.js
+++ m3/create-bill/create-bill-4.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-    sessionStorage.setItem("selected-category", 'Groceries');
+$(document).ready(function () {
+  sessionStorage.setItem("selected_category", "Groceries");
 
-    $('.upload-file-button').click(function () {
-        var category = prompt("New category name: (Max 20 chars)");
-        category = category.substring(0, 20);
-        $(".categories").append("<div class='category inter-bold-black-12px' onclick='selectCategory(\"" + category + "\")'>" + category + "</div>");
-    });
-    
+  $(".upload-file-button").click(function () {
+    var category = prompt("New category name: (Max 20 chars)");
+    category = category.substring(0, 20);
+    $(".categories").append(
+      "<div class='category inter-bold-black-12px' onclick='selectCategory(\"" +
+        category +
+        "\")'>" +
+        category +
+        "</div>"
+    );
+  });
 });
-  
